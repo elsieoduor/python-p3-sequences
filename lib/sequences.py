ceb220034971d97a4242cf2a3e79d49f89bd1efc
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-18 14:28:21 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        return []
+    sequence = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    while len(sequence) < length:
+        next = sequence[-1] + sequence[-2] 
+        sequence.append(next)
+    else:
+        return(print_fibonacci(length - 1) + print_fibonacci(length - 2))
+
+print(print_fibonacci(9))
+
